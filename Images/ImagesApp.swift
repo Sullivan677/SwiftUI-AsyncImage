2963df5a93c1c999c3731ea36415aa7c0eea22d6
Sullivan677 <decarli.sulli@gmail.com>
2021-11-01 16:53:27 +0100

    Initial Commit

---
+++ Images/ImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImagesApp.swift
+//  Images
+//
+//  Created by Sullivan De carli on 01/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct ImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
